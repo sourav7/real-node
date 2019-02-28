e2a05c685943c0f7110bfa95faa2d513d6faf70a
sourav7 <souravdebnath7@gmail.com>
2019-02-28 15:39:27 +0600

    simple html server created

---
+++ simpleserver/server.js
@@ -0,0 +1,56 @@
+const http = require('http');
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
+
+const mimeTypes = {
+    "html": "text/html",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpg",
+    "png": "image/png",
+    "js": "text/javascript",
+    "css": "text/css"
+}
+http.createServer((req, res) => {
+    let uri = url.parse(req.url).pathname;
+    let fileName = path.join(process.cwd(), unescape(uri));
+    console.log(`Loading ${uri}`);
+    let stats;
+
+    try {
+        stats = fs.lstatSync(fileName);
+    } catch (e) {
+        res.writeHead(404, 'Page not found', { 'Content-type': 'text/plain' });
+        res.write('404 not found');
+        res.end();
+        return;
+    }
+    if (stats.isFile()) {
+        let mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
+        console.log(`Mimetype is ${mimeType}`);
+
+        res.writeHead(200, { 'Content-type': mimeType });
+        var fileStream = fs.createReadStream(fileName);
+        fileStream.pipe(res);
+    } else if (stats.isDirectory()) {
+        res.writeHead(302, { 'Location': 'index.html' });
+        res.end();
+    } else {
+        res.writeHead(500, { 'Content-type': 'text/plain' });
+        res.write('500 internal server error');
+        res.end();
+    }
+}).listen(3000);
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello World\n');
+// });
+
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// });
